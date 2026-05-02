e141a6a3d708eff33e6fbd51c1a6baa4c468f7b2
Pappt04 <69700151+Pappt04@users.noreply.github.com>
2026-05-02 21:04:21 +0200

    feat: Navigation with gestures and navigation drawer with basic fields

--- app/src/main/java/uns/ac/rs/team23/slagalica/views/MainActivity.kt
+++ app/src/main/java/uns/ac/rs/team23/slagalica/views/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import org.koin.androidx.compose.koinViewModel
 import uns.ac.rs.team23.slagalica.ui.theme.SlagalicaTheme
-import uns.ac.rs.team23.slagalica.viewmodels.AuthViewModel
-import uns.ac.rs.team23.slagalica.viewmodels.UserSession
-import uns.ac.rs.team23.slagalica.views.welcome.WelcomePage
+import uns.ac.rs.team23.slagalica.views.navigation.AppNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,20 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SlagalicaTheme {
-                val authViewModel: AuthViewModel = koinViewModel()
-                val userSession by authViewModel.userSession.collectAsState()
-
-                when (userSession) {
-                    is UserSession.NotLoggedIn -> WelcomePage(viewModel = authViewModel)
-                    is UserSession.Guest -> HomeScreen(
-                        username = "Guest",
-                        onLogout = authViewModel::logout,
-                    )
-                    is UserSession.LoggedIn -> HomeScreen(
-                        username = (userSession as UserSession.LoggedIn).username,
-                        onLogout = authViewModel::logout,
-                    )
-                }
+                AppNavHost()
             }
         }
     }
