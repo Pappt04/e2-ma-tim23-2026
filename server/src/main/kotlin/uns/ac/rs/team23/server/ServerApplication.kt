8ad7672d178706707b1003b0e5e27872eb2c2667
Pappt04 <69700151+Pappt04@users.noreply.github.com>
2026-06-05 22:11:14 +0200

    Added server component

---
+++ server/src/main/kotlin/uns/ac/rs/team23/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package uns.ac.rs.team23.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerApplication>(*args)
+}
