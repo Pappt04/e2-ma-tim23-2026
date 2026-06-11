a165a4c326ee6e5d6564225daf8ec93867546f5c
Pappt04 <69700151+Pappt04@users.noreply.github.com>
2026-06-11 17:11:33 +0200

    full match

--- slagalica/app/src/main/java/uns/ac/rs/team23/slagalica/data/MatchStore.kt
+++ slagalica/app/src/main/java/uns/ac/rs/team23/slagalica/data/MatchStore.kt
@@ -1,20 +1,27 @@
 package uns.ac.rs.team23.slagalica.data
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 object MatchStore {
     var matchId: String = ""
     var opponentUsername: String = ""
     var isFriendly: Boolean = false
+    var currentGameIndex by mutableStateOf(0)
 
     fun set(id: String, opponent: String, friendly: Boolean = false) {
         matchId = id
         opponentUsername = opponent
         isFriendly = friendly
+        currentGameIndex = 0
     }
 
     fun clear() {
         matchId = ""
         opponentUsername = ""
         isFriendly = false
+        currentGameIndex = 0
     }
 
     val isActive: Boolean get() = matchId.isNotBlank()
